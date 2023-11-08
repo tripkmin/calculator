6b9e6cedae137886c00bf1c535039da87a1589dd
tripkmin <tripkmin@gmail.com>
2023-11-08 20:56:36 +0900

    fix: Fix styled components global setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div>
-      <h1>Calc</h1>
+      <p>Calc</p>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -10,11 +10,9 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={{ color: 'red' }}>
-      <GlobalFont>
-        <GlobalStyles>
-          <App />
-        </GlobalStyles>
-      </GlobalFont>
+      <GlobalFont />
+      <GlobalStyles />
+      <App />
     </ThemeProvider>
   </React.StrictMode>
 );
