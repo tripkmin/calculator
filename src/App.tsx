1da270b43ffb45b0c6e640b7d9e072e46dc4451e
tripkmin <tripkmin@gmail.com>
2023-11-12 02:57:08 +0900

    Fix: Resolved layout shift during theme change

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
 import { ThemeProvider } from 'styled-components';
-import GlobalFont from 'assets/fonts/GlobalFont';
 import GlobalStyles from 'styles/GlobalStyles';
 import Calculator from 'components/Calculator';
 import Header from 'assets/Layouts/Header';
 import Main from 'assets/Layouts/Main';
 import { themeType } from 'styles/constants';
 import useTheme from 'hooks/useTheme';
+import './index.css';
 
 function App() {
   const { theme, setTheme } = useTheme();
 
   return (
     <ThemeProvider theme={themeType[theme]}>
-      <GlobalFont />
       <GlobalStyles />
       <section>
         <Header theme={theme} setTheme={setTheme} />
